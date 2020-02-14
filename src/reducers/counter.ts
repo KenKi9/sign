f00fc6c2abee3f91593066d668700acd3b5f1acf
KenKi9 <1595953919@qq.com>
2020-02-14 21:31:14 +0800

    map

--- src/reducers/counter.ts
+++ src/reducers/counter.ts
@@ -1,17 +1,15 @@
-import { ADD, MINUS } from '../constants/counter'
-
 const INITIAL_STATE = {
   num: 0
 }
 
 export default function counter (state = INITIAL_STATE, action) {
   switch (action.type) {
-    case ADD:
+    case 'ADD':
       return {
         ...state,
         num: state.num + 1
       }
-     case MINUS:
+     case 'MINUS':
        return {
          ...state,
          num: state.num - 1
